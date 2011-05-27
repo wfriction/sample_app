a88afbbe30e15d6fd1138ae014f101547ceb02fb
Chris <chris@withoutfriction.com>
2011-05-27 17:47:27 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ch3::Application.config.secret_token = 'cca744a4ca08f3d7e66e4220f57f78c0a51efcca474829f80e8c8478906e5952d5d74848c11c675df49aee33c484b39f490e3dd6e4ba342722ddcd01f18a71c7'
