edf0fa63c4fdf15fa4b03fbb161f540759e56eaa
Chris <chris@withoutfriction.com>
2011-05-28 23:44:03 +1000

    Changed line alignment

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,7 +15,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => @base_title + "Home")
+      response.should have_selector("title",
+                                    :content => @base_title + "Home")
     end
   end
 
@@ -27,7 +28,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => @base_title + "Contact")
+      response.should have_selector("title",
+                                    :content => @base_title + "Contact")
     end
   end
 
@@ -39,7 +41,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => @base_title + "About")
+      response.should have_selector("title",
+                                    :content => @base_title + "About")
     end
   end
 
@@ -51,7 +54,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title", :content => @base_title + "Help")
+      response.should have_selector("title",
+                                    :content => @base_title + "Help")
     end
   end
 end
